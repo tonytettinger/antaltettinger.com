750fcc47b89ca3671be19864cba689248ba5fd24
tonytettinger <antal.tettinger@gmail.com>
2020-04-30 06:13:08 +0200

    Update build

---
+++ build/precache-manifest.b9db6078e943d6224bfb3e008bfaa671.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a814f3cd0038c1214fb000227e42e91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5599ec062c639a6ba79",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c17c03ef84b2846f96b1",
+    "url": "/static/js/2.6874053b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6874053b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5599ec062c639a6ba79",
+    "url": "/static/js/main.6e722218.chunk.js"
+  },
+  {
+    "revision": "6f208d30d0f6dc6466e8",
+    "url": "/static/js/runtime-main.90632f4d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
